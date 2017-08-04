1b828b0be5e864c27423619f13fd93dad09e8ad5
sashank27 <sashankmishra27@gmail.com>
2017-08-04 16:14:03 +0530

    Resources fragment updated

--- app/src/main/java/com/sashank/iiitamun/Fragments/ResourcesFragment.java
+++ app/src/main/java/com/sashank/iiitamun/Fragments/ResourcesFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.TextView;
 
 import com.sashank.iiitamun.R;
 
